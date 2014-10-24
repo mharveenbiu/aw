6bd5f531875873e288686155323bda6e15819885
mharveenbiu <mharveen@gmail.com>
2014-10-24 18:03:13 +0800

    Upload filed

---
+++ FinalProj/bodyparts.js
@@ -0,0 +1,39 @@
+var circulatory_system = 
+{
+    diseases : {
+				minor1: {
+						name:"Chronic Venous Insufficiency",
+						description:"Chronic venous insufficiency (CVI) is a condition that occurs when the venous wall and/or valves in the leg veins are not working effectively, making it difficult for blood to return to the heart from the legs. CVI causes blood to “pool” or collect in these veins, and this pooling is called stasis.",
+						symptoms:["Swelling in the lower legs and ankles, especially after extended periods of standing","Aching or tiredness in the legs","New varicose veins","Leathery-looking skin on the legs","Flaking or itching skin on the legs or feet","Stasis ulcers (or venous stasis ulcers)"],
+						treatments: {
+									Compression_Stockings : "The most conservative approach is to wear properly-fitting support hose (also called compression stockings). Compression stockings can be purchased at some pharmacies and medical supply stores and come in various styles, including below-the-knee, above-the-knee and pantyhose styles. They also come in different compressions varying from 8 to 10 mm Hg, up to 40 to 50 mm Hg. Your doctor can recommend the compression that is right for you. You will need a prescription for any stockings with more than 20 mm Hg compression. If you wear compression stockings, be sure to take them off at the end of the day to wash and dry them, and to clean and check your skin. Make sure the stockings fit so there is no bunching. Elastic stockings that fit poorly can actually make your condition worse by blocking blood flow in the area where they have bunched up. Some studies have shown that combining elastic socks with prescription medication to improve blood flow is very effective when the socks alone do not control symptoms.",
+									
+									Antibiotics: "Antibiotics may be prescribed to clear skin infections related to CVI, but the underlying disease must be treated to prevent a recurrence. Deeper infections and ulcers may also be treated with antibiotics.",
+									
+									Other_Medications: "If you have post-thrombotic syndrome, your doctor may prescribe medication to prevent the formation of additional blood clots. A special medicated wrap, known as an Unna Boot, combines multilayer compression with a zinc oxide gel-based wound cover that forms a semi-rigid bandage. Other multilayer compression systems are available and are often used in combination with topical wound care products. Some patients have found benefit from the herbal dietary supplement Vena-Stat, which contains a derivative of horse chestnut extract. Keep in mind that herbal preparations should not be used in place of your prescription medications and should be used with caution, as they may interact with current prescription medications. Please ask your doctor or pharmacist about any potential drug interactions.",
+									
+									Skin_Care : "Practicing good skin hygiene is important. Keep your skin moisturized so that it doesn’t flake or crack easily. If the skin is not broken or leaking fluid but is inflamed, your doctor may recommend an anti-itch cream, such as one containing hydrocortisone; a cream containing zinc oxide to protect the skin; or an antifungal cream to prevent fungal infections. Skin that is leaking fluid is treated with wet compresses. If you have ulcers on your legs, your doctor will show you how to apply layered compression bandages to protect the skin and maintain blood flow.",
+									
+									Nonsurgical_Treatment : " Nonsurgical treatments include sclerotherapy and endovenous thermal ablation.",
+									
+									Sclerotherapy : "involves the injection of a solution directly into spider veins or small varicose veins that causes them to collapse and disappear. Several sclerotherapy treatments are usually required to achieve the desired results. Sclerotherapy is simple, relatively inexpensive, and can be performed in the doctor’s office. Sclerotherapy can eliminate the pain and discomfort of these veins and helps prevent complications such as venous hemorrhage and ulceration. It is also frequently performed for cosmetic reasons.",
+
+									Endovenous_thermal_ablation : "is a newer technique that uses a laser or high-frequency radio waves to create intense local heat in the affected vein. The technology is different with each energy source, but both forms of local heat close up the targeted vessel. This treatment closes off the problem veins but leaves them in place so there is minimal bleeding and bruising. Compared with ligation and stripping, endovenous thermal ablation results in less pain and a faster return to normal activities, with similar cosmetic results."
+									},
+						picture: "Diseases/Circulatory/Chronic-Venous-Insufficiency.jpg"
+						},
+				minor2:{
+						name:"Atherosclerosis",
+						description:"Literally, “hardening of the fatty stuff.” High fat diets can lead to formation of fatty plaques lining blood vessels. These fatty areas can become calcified and hard leading to arteriosclerosis, hardening of the arteries. When blood vessels become less stretchable, blood pressure rises and can result in heart and kidney damage and strokes. Double cheese bacon burger, anybody?",
+						symptoms:["Angina - is chest pain or discomfort that occurs when your heart muscle doesn't get enough oxygen-rich blood.","shortness of breath","Arrhythmias - are problems with the rate or rhythm of the heartbeat."],
+						treatments: {
+									Physically_Active: "Regular physical activity can lower many atherosclerosis risk factors, including LDL bad cholesterol, high blood pressure, and excess weight.Physical activity also can lower your risk for diabetesexternal link icon and raise your HDL cholesterol level. HDL is the good cholesterol that helps prevent atherosclerosis.",
+									Healthy_Weight: "Maintaining a healthy weight can lower your risk for atherosclerosis. A general goal to aim for is a body mass index (BMI) of less than 25",
+									Quit_Smoking:"Smoking can damage and tighten blood vessels and raise your risk for atherosclerosis. Talk with your doctor about programs and products that can help you quit. Also, try to avoid secondhand smoke",
+									Manage_Stress:"Learning how to manage stressexternal link icon, relax, and cope with problems can improve your emotional and physical health. Having supportive people in your life with whom you can share your feelings or concerns can help relieve stress."									
+									},
+						picture: ["Diseases/Circulatory/ather_lowres.jpg","Diseases/Circulatory/angina-pectoris.jpg"]
+						}
+				}
+};
+
